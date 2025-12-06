921d7c281706b2591748863a9465aff28ca85ccc
faris <faris.rawls5@gmail.com>
2025-12-05 20:43:02 -0500

    Delete linear.asm

--- linear.asm
+++
@@ -1,8 +0,0 @@
-; Just a linear sequence of instructions;
-; No jumps, no branching.
-LOADI R1, #0x01
-LOADI R2, #0x02
-ADD R3, R1, R2      ; r3 <-- 1 + 2
-ADD R4, R3, R1      ; r4 <-- 3 + 1
-ADDI R5, R4 #0x0A   ; r5 <-- 4 + 10
-HALT
